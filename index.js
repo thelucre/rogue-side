745b23f865d90d2d9d9100e008de5417c8ed1517
Eric Howard <howard.eric.m@gmail.com>
2015-08-31 19:59:57 -0700

    basic socket chat

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Rogue Side</h1>');
+  res.sendFile(__dirname + '/public/index.html');
 });
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
 
 // http.listen(3000, function(){
 //   console.log('listening on *:3000');
